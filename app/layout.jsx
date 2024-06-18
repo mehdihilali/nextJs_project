0bcba529f44351a88c7eba4d66561ddd2ee107b8
elhilali.elmehdi.edu@gmail.com <elhilali.elmehdi.edu@gmail.com>
2024-06-18 22:27:51 +0100

    The use of use client and some features

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import '@/assets/styles/globals.css';
-import React from 'react'
+import React from 'react';
+
+export const metadata = {
+    title: 'PropertyPulse | Find The Perfect Rental',
+    description: 'Find Your Dream Rental Property',
+    keywords: 'rental, find retals, find properties',
+};
 
 const MainLayout = ({ children }) => {
   return (
