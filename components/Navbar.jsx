cafe987cee5dd79d7a890884b2e4b6bda4d87273
elhilali.elmehdi.edu@gmail.com <elhilali.elmehdi.edu@gmail.com>
2024-06-19 01:22:39 +0100

    Active Links - isLogged in or not and other features

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
 'use client';
+import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 import React from 'react'
 import Image from 'next/image';
@@ -10,6 +11,9 @@ import { FaGoogle } from 'react-icons/fa'
 const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const pathname = usePathname();
 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -61,40 +65,49 @@ const Navbar = () => {
             </Link>
             {/* <!-- Desktop Menu Hidden below md screens --> */}
             <div className="hidden md:ml-6 md:block">
-              <div className="flex space-x-2">
-                <Link
-                  href="/"
-                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</Link
-                >
-                <Link
-                  href="/properties"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Properties</Link
-                >
-                <Link
-                  href="/properties/add"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Property</Link
-                >
-              </div>
+                <div className="flex space-x-2">
+                    <Link
+                        href="/"
+                        className={`${pathname === '/' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
+                    >
+                    Home
+                    </Link>
+                    <Link
+                        href="/properties"
+                         className={`${pathname === '/properties' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
+                    >
+                    Properties
+                    </Link>
+                    { isLoggedIn && (
+                        <Link
+                            href="/properties/add"
+                            className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
+                        >
+                        Add Property
+                        </Link>
+                    )}
+                    
+                </div>
             </div>
-          </div>
+        </div>
 
           {/* <!-- Right Side Menu (Logged Out) --> */}
-          <div className="hidden md:block md:ml-6">
-            <div className="flex items-center">
-              <button
-                className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-              >
-                <FaGoogle className='text-white mr-2' />
-                <span>Login or Register</span>
-              </button>
+          { !isLoggedIn && (
+            <div className="hidden md:block md:ml-6">
+                <div className="flex items-center">
+                    <button
+                        className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    >
+                        <FaGoogle className='text-white mr-2' />
+                        <span>Login or Register</span>
+                    </button>
+                </div>
             </div>
-          </div>
-
+          )}
+          
           {/* <!-- Right Side Menu (Logged In) --> */}
-          <div
+          { isLoggedIn && (
+            <div
             className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0"
           >
             <Link href="/messages" className="relative group">
@@ -184,6 +197,8 @@ const Navbar = () => {
               )}
             </div>
           </div>
+          )}
+          
         </div>
       </div>
 
@@ -193,25 +208,30 @@ const Navbar = () => {
         <div className="space-y-1 px-2 pb-3 pt-2">
           <Link
             href="/"
-            className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+            className= {`${pathname === '/' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
             >Home</Link
           >
           <Link
             href="/properties"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            className= {`${pathname === '/properties' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
             >Properties
             </Link>
-          <Link
+            { isLoggedIn && (
+                <Link
             href="/properties/add"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            className= {`${pathname === '/properties/add' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
             >Add Property
             </Link>
-          <button
-            className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4"
-          >
-            <FaGoogle className='text-white mr-2' />
-            <span>Login or Register</span>
-          </button>
+            )}
+          
+            {!isLoggedIn && (
+                <button
+                    className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4"
+                 >
+                    <span>Login or Register</span>
+                </button>
+            )}
+          
         </div>
       </div>
       )}
