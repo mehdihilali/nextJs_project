0bf9117de92409209d2fd7f934e95099649768ae
elhilali.elmehdi.edu@gmail.com <elhilali.elmehdi.edu@gmail.com>
2024-06-20 21:54:37 +0100

    Fetch Single Property

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,8 +1,33 @@
 'use client';
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { fetchProperty } from '@/utils/requests';
 
 import React from 'react'
 
 const PropertyPage = () => {
+  const { id } = useParams();
+
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) return;
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.error('Error fetching property :', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if (property === null){
+      fetchPropertyData();
+    }
+  },[id, property])
 
   return (
     <div>
--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,7 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+
+// Fetch All Properties
 async function fetchProperties() {
     try {
         // Handle the case where the domain is not available yet 
@@ -23,4 +25,28 @@ async function fetchProperties() {
     }
   }
 
-  export { fetchProperties };
+  // Fetch Single Property
+  async function fetchProperty(id) {
+    try {
+        // Handle the case where the domain is not available yet 
+        if (!apiDomain){
+            return null;
+        }
+      // Generate a unique cache-busting query parameter
+      const cacheBuster = new Date().getTime(); // Current timestamp
+  
+      const res = await fetch(`${apiDomain}/properties/${id}?cache=${cacheBuster}`);
+  
+      if (!res.ok) {
+        throw new Error('Failed to fetch data');
+      }
+  
+      return res.json();
+  
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  export { fetchProperties, fetchProperty };
