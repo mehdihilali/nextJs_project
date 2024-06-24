88e9f80e367160a6fe769c4355fe6ec090599707
elhilali.elmehdi.edu@gmail.com <elhilali.elmehdi.edu@gmail.com>
2024-06-24 16:47:29 +0100

    Protect URL by adding midlleware from next-auth

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,6 +11,8 @@ import { signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Navbar = () => {
     const { data: session} = useSession();
+    const profileImage = session?.user?.image;
+
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
     const [providers, setProviders] = useState(null);
@@ -24,6 +26,7 @@ const Navbar = () => {
     }
     setAuthProviders();
   }, []);
+
 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -167,8 +170,10 @@ const Navbar = () => {
                   <span className="sr-only">Open user menu</span>
                   <Image
                     className="h-8 w-8 rounded-full"
-                    src= {profileDefault}
+                    src= {profileImage || profileDefault}
                     alt=""
+                    width={40}
+                    height={40}
                   />
                 </button>
               </div>
@@ -189,6 +194,9 @@ const Navbar = () => {
                   role="menuitem"
                   tabIndex="-1"
                   id="user-menu-item-0"
+                  onClick={() => {
+                    setIsProfileMenuOpen(false);
+                  }}
                   >Your Profile</Link
                 >
                 <Link
@@ -197,9 +205,16 @@ const Navbar = () => {
                   role="menuitem"
                   tabIndex="-1"
                   id="user-menu-item-2"
+                  onClick={() => {
+                    setIsProfileMenuOpen(false);
+                  }}
                   >Saved Properties</Link
                 >
                 <button
+                  onClick={() => {
+                    setIsProfileMenuOpen(false);
+                    signOut();
+                  }}
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
                   tabIndex="-1"
