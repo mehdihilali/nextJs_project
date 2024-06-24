bfb1a4b1fed586ce88122ab6e90f268de6345daf
elhilali.elmehdi.edu@gmail.com <elhilali.elmehdi.edu@gmail.com>
2024-06-24 20:23:17 +0100

    Uploading images to Cloudinary

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,6 +1,7 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
+import cloudinary from "@/config/cloudinary";
 
 
 // GET /api/properties
@@ -63,8 +64,34 @@ export const POST = async (request) => {
                 email: formData.get('seller_info.email'),
                 phone: formData.get('seller_info.phone')
             },
-            owner: userId,
-            //images
+            owner: userId
+        };
+
+        // Upload images to Cloudinary: 
+        const imagesUploadsPromises = [];
+
+        for(const image of images) {
+            const imageBuffer = await image.arrayBuffer();
+            const imageArray = Array.from(new Uint8Array(imageBuffer));
+            const imageData = Buffer.from(imageArray);
+
+            // Convert the image data to base64:
+            const imageBase64 = imageData.toString('base64');
+
+            // Make request to upload to cloudinary:
+            const result = await cloudinary.uploader.upload(
+                `data:image/png;base64,${imageBase64}`,{
+                    folder: 'propertypulse'
+                }
+            );
+
+            imagesUploadsPromises.push(result.secure_url);
+
+            // wait for all images to upload :
+            const uploadedImages = await Promise.all(imagesUploadsPromises);
+
+            // Add uploaded images to propertyData object
+            propertyData.images = uploadedImages;
         }
 
         const newProperty = new Property(propertyData);
