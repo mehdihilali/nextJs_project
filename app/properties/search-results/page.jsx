0e75550870346adfcb5acc0d4ca4cbee954846ce
elhilali.elmehdi.edu@gmail.com <elhilali.elmehdi.edu@gmail.com>
2024-06-30 00:20:39 +0100

    Display Search Results

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -2,6 +2,11 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+import { FaArrowAltCircleLeft } from 'react-icons/fa';
+import PropertyCard from '@/components/PropertyCard';
+import Spinner from '@/components/Spinner';
+import PropertySearchForm from '@/components/PropertySearchForm';
 
 const SearchResultsPage = () => {
 
@@ -33,9 +38,38 @@ const SearchResultsPage = () => {
 
         fetchSearchResult();
     },[location, propertyType]);
-    console.log(properties);
   return (
-    <div>SearchResultsPage</div>
+    <>
+        <section className="bg-blue-700 py-4">
+            <div className="nax-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+                <PropertySearchForm />
+            </div>
+        </section>
+        { 
+            loading ? (<Spinner loading={loading} />) : (
+                <section className="px-4 py-6">
+                  <div className="container-xl lg:container m-auto px-4 py-6">
+                    <Link href='/properties' className='flex items-center text-blue-500 hover:underline mb-3'>
+                        <FaArrowAltCircleLeft className='mr-2 mb-1' />Back to properties
+                    </Link>
+                    <h1 className="text-2xl mr-4">Search Results</h1>
+            
+                    {/*That means : if there is no properties then show the paragraph that contains the message "No properties found" , else show all properties that exist in properties.json*/}
+                    {/*If length of properties is null Then (?) ... Else (:)*/}
+                    {properties.length === 0 ? (
+                      <p>No Search Results Found</p>
+                    ) : (
+                      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                      {properties.map((property) => (
+                        <PropertyCard key={property._id} property= {property} />
+                      ))}
+                    </div>
+                    )}
+                  </div>
+                </section>
+              )
+        }
+    </>
   )
 }
 
