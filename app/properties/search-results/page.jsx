6c2b37ff010c5ace70152598c83d55ce99f6ac2d
elhilali.elmehdi.edu@gmail.com <elhilali.elmehdi.edu@gmail.com>
2024-06-29 23:49:21 +0100

    Fatch Results From API

---
+++ app/properties/search-results/page.jsx
@@ -0,0 +1,42 @@
+'use client';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+const SearchResultsPage = () => {
+
+    const searchParams = useSearchParams();
+
+    const [properties , setProperties] = useState([]);
+    const [loading , setLoading] = useState(true);
+
+    const location = searchParams.get('location');
+    const propertyType = searchParams.get('propertyType');
+
+    useEffect(() => {
+        const fetchSearchResult = async () => {
+            try {
+                const res = await fetch(`/api/properties/search?location=${location}&propertyType=${propertyType}`);
+
+                if (res.status === 200) {
+                    const data = await res.json();
+                    setProperties(data);
+                } else {
+                    setProperties([]);
+                }
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchSearchResult();
+    },[location, propertyType]);
+    console.log(properties);
+  return (
+    <div>SearchResultsPage</div>
+  )
+}
+
+export default SearchResultsPage
